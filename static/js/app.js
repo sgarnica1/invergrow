08bb031ea9b594e7aaf552a3d93dd4cb04157c87
Sergio Garnica <sergio190201@gmail.com>
2021-02-05 13:54:10 -0600

    Header terminado

---
+++ static/js/app.js
@@ -0,0 +1,52 @@
+const menuBttn = document.querySelector('.menu-btn');
+const closeBttn = document.querySelector('.cancel-btn');
+const nav = document.querySelector('.navbar_list');
+const header = document.querySelector('.header');
+const logo = document.querySelector('.navbar_logo').firstElementChild;
+const menuLinks = document.querySelectorAll('.navbar_list_item');
+
+menuBttn.addEventListener('click', () => {
+  nav.classList.add('active');
+})
+closeBttn.addEventListener('click', () => {
+  nav.classList.remove('active');
+})
+
+const scrollChangeTextColor = () => {
+  for(let el of menuLinks) {
+    if(el.firstElementChild.classList.contains('active')){
+      continue
+    } else {
+      el.firstElementChild.classList.add('toggle')
+    }
+  }
+}
+
+const changeTextColor = () => {
+  for(let el of menuLinks) {
+    if(el.firstElementChild.classList.contains('active')){
+      continue
+    } else {
+      el.firstElementChild.classList.remove('toggle')
+    }
+  }
+}
+
+window.onscroll = () => {
+  // this.scrollY > 20 ? header.classList.add('sticky') : header.classList.remove('sticky')
+  if(this.scrollY > 20) {
+    header.classList.add('sticky');
+    logo.src = "../static/logo/invergrow_logo_recortado.png";
+    // logo.classList.add('img-sticky');
+    menuBttn.classList.add('toggle');
+    menuLinks[0].firstElementChild.classList.add('active');
+    scrollChangeTextColor();
+  } else {
+    header.classList.remove('sticky');
+    logo.src = "../static/logo/invergrow_logo_recortado_blanco.png";
+    // logo.classList.remove('img-sticky');
+    menuBttn.classList.remove('toggle');
+    changeTextColor();
+    // menuLinks[0].firstElementChild.classList.remove('active');
+  }
+}
