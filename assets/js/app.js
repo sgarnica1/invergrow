92af6f6c9d57381a97e90b375d06b92a712916b4
Sergio Garnica <sgarnica1902@gmail.com>
2021-09-09 12:20:08 -0500

    Changes in header, banner, homepage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,14 @@
+const scrollHeader = document.querySelector('.header-scroll')
 const menuBttn = document.querySelector('.menu-btn');
-const closeBttn = document.querySelector('.cancel-btn');
-const nav = document.querySelector('.navbar_list');
+const menuBtns = document.querySelectorAll('.menu-btn')
+
+const closeBttn = document.querySelector('.cancel-btn.mobile');
+const nav = document.querySelector('.navbar_list.mobile');
 const header = document.querySelector('.header');
-const logo = document.querySelector('.navbar_logo').firstElementChild;
+const logo = document.querySelector('.navbar__logo').firstElementChild;
 const navBarSubMenu = document.querySelector('.navbar_submenu_parent');
 const subMenu = document.querySelector('.navbar_submenu');
 const menuLinks = document.querySelectorAll('.navbar_list_item');
-const subMenuDropBttn = document.getElementById('dropdown-menu');
 const phoneHeaderLink = document.querySelector('.header_phone_link');
 const topBttn = document.getElementById('top-bttn');
 const whatsAppBttn = document.getElementById('whatsapp-bttn');
@@ -20,49 +22,6 @@ const carouselBttnNextPrevMoreImgs = document.querySelectorAll('.carousel_bttn_m
 const prodcutImg = document.querySelectorAll('.product_img');
 
 
-window.onscroll = () => {
-  // this.scrollY > 20 ? header.classList.add('sticky') : header.classList.remove('sticky')
-  if(this.scrollY > 20) {
-    header.classList.add('sticky');
-    logo.src = "./assets/logo/invergrow_logo_recortado.png";
-    menuBttn.firstElementChild.style.backgroundImage = "url('./assets/icon/menu.svg')"
-    menuLinks[0].firstElementChild.classList.add('active');
-    subMenuDropBttn.style.color = "var(--main-font-color)"
-    scrollChangeTextColor();
-  } else {
-    header.classList.remove('sticky');
-    logo.src = "./assets/logo/invergrow_logo_recortado_blanco.png";
-    menuBttn.firstElementChild.style.backgroundImage = "url('./assets/icon/menu_white.svg')"
-    subMenuDropBttn.style.color = "white"
-    changeTextColor();
-  }
-  if (this.scrollY > 150) {
-    if(nav.className.includes('active')) {
-      hideBttns();
-    } else {
-      showBttns();
-    }
-  } else {
-    hideBttns();
-  }
-}
-
-
-const scrollChangeTextColor = () => {
-  for(let el of menuLinks) {
-    el.firstElementChild.classList.add('toggle')
-  }
-  phoneHeaderLink.classList.add('toggle');
-}
-
-const changeTextColor = () => {
-  for(let el of menuLinks) {
-    el.firstElementChild.classList.remove('toggle')
-  }
-  phoneHeaderLink.classList.remove('toggle');
-};
-
-
 //WhatsApp and To Top Bttn Show and Hide
 const showBttns = () => {
   topBttn.classList.remove('hide');
@@ -74,18 +33,32 @@ const hideBttns = () => {
   whatsAppBttn.classList.add('hide');
 }
 
+window.onscroll = () => {
+  if (this.scrollY > 150) {
+    scrollHeader.classList.add('active')
+    if(nav.className.includes('active')) {
+      hideBttns();
+    } else {
+      showBttns();
+    }
+  } else {
+    scrollHeader.classList.remove('active')
+    hideBttns();
+  }
+}
+
 
 //Mobile Menu
-menuBttn.addEventListener('click', () => {
-  nav.classList.add('active');
-  hideBttns();
-  subMenuDropBttn.style.color = "white";
-  document.body.classList.add('block-scroll');
-})
+for(let btn of menuBtns) {
+  btn.addEventListener('click', () => {
+    nav.classList.add('active');
+    hideBttns();
+    document.body.classList.add('block-scroll');
+  })
+}
 
 closeBttn.addEventListener('click', () => {
   nav.classList.remove('active');
-  subMenu.classList.add('hide');
   fb.style.display = "inline-block";
   ig.style.display = "inline-block";
   document.body.classList.remove('block-scroll');
@@ -93,60 +66,6 @@ closeBttn.addEventListener('click', () => {
     showBttns();
   }
 })
-
-
-//Submenu
-navBarSubMenu.addEventListener('click', () => {
-  subMenu.classList.toggle('hide');
-  if(subMenu.className.includes('hide')) {
-    subMenuDropBttn.style.color = "white"
-    subMenuDropBttn.classList.remove('fa-angle-up');
-    subMenuDropBttn.classList.add('fa-angle-down');
-    fb.style.display = "inline-block";
-    ig.style.display = "inline-block";
-  } else {
-    subMenuDropBttn.style.color = "var(--extra-color)"
-    subMenuDropBttn.classList.remove('fa-angle-down');
-    subMenuDropBttn.classList.add('fa-angle-up');
-    fb.parentElement.style.position = "relative";
-  }
-})
-
-navBarSubMenu.addEventListener('mouseover', () => {
-  header.classList.add('sticky');
-  logo.src = "./assets/logo/invergrow_logo_recortado.png";
-  menuBttn.firstElementChild.style.backgroundImage = "url('./assets/icon/menu.svg')"
-  menuBttn.firstElementChild.style.backgroundImage = "url('./assets/icon/menu.svg')"
-  menuLinks[0].firstElementChild.classList.add('active');
-  subMenuDropBttn.style.color = "var(--tertiary-font-color)"
-  subMenuDropBttn.classList.remove('fa-angle-down');
-  subMenuDropBttn.classList.add('fa-angle-up');
-  scrollChangeTextColor();
-})
-
-navBarSubMenu.addEventListener('mouseout', () => {
-  subMenuDropBttn.classList.add('fa-angle-down');
-  subMenuDropBttn.classList.remove('fa-angle-up');
-  if(this.scrollY > 20) {
-    subMenuDropBttn.style.color = "var(--main-font-color)";
-  }
-  if(this.scrollY < 20) {
-    header.classList.remove('sticky');
-    logo.src = "./assets/logo/invergrow_logo_recortado_blanco.png";
-    menuBttn.firstElementChild.style.backgroundImage = "url('./assets/icon/menu_white.svg')"
-    subMenuDropBttn.style.color = "white"
-    changeTextColor();
-  }
-})
-
-
-//WhastApp bttn
-// whatsAppBttn.addEventListener('mouseover', () => {
-//   whatsAppBttn.firstElementChild.firstElementChild.src = './assets/icon/whatsapp_1.svg';
-// })
-// whatsAppBttn.addEventListener('mouseout', () => {
-//   whatsAppBttn.firstElementChild.firstElementChild.src = './assets/icon/whatsapp_2.svg';
-// })
 
 
 //Footer Date
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <h1 class="banner_title">¡Cotiza ya los mejores precios del mercado!</h1>
         <hr class="banner_divider">
         <h2 class="banner_subtitle">Conoce Invergrow</h2>
-        <a href="#categories" class="banner_bttn">Ver catálogo</a>
+        <a href="./agricultura-protegida" class="banner_bttn">Ver Productos</a>
       </div>
     </section>
 
@@ -25,13 +25,7 @@
       </div>
       <section class="categories_cards">
         <a href="./agricultura-protegida" class="categories_card cat1">
-          <h3>Insumos: Agricultura Protegida</h4>
-        </a>
-        <a href="./campo-abierto" class="categories_card cat2">
-          <h3>Insumos: Campo Abierto</h4>
-        </a>
-        <a href="./montacargas" class="categories_card cat3">
-          <h3>Venta y renta de montacargas</h4>
+          <h3>Insumos Agricultura Protegida</h4>
         </a>
       </section>
       </section>
@@ -49,80 +43,8 @@
             <img src="./assets/img/cat_des_2_mb.png" alt="Imagen de invernadero con plantas color" class="category_description_img">
           </div>
         </div>
-        <div class="category_description category_2">
-          <div class="category_description_info">
-            <div class="category_description_info_text">
-              <h2>2</h2>
-              <h3>Insumos: Campo Abierto</h3>
-              <p>Conoce todos nuestros insumos para el campo, desde tractores de diferentes marcas como Kubota, Case in y New Holland, hasta implementos agrícolas y refacciones.</p>
-              <div class="button-border button-border-2">
-                <a href="./campo-abierto" class="category_description_button description_button_2">Ver catálogo</a>
-              </div>
-            </div>
-            <img src="./assets/img/cat_des_3_mb.png" alt="Imagen de un tractor grande, color verde con llantas negras con rines rojos" class="category_description_img">
-          </div>
-        </div>
-        <div class="category_description category_3">
-          <div class="category_description_info">
-            <h2>3</h2>
-            <h3>Venta y renta de montacargas</h3>
-            <p>Encuentra con nosotors todo tipo de montacargas que necesites. Para venta o renta. ¡Pregúntanos!</p>
-            <div class="button-border button-border-3">
-              <a href="./montacargas" class="category_description_button description_button_3">Ver catálogo</a>
-            </div>
-            <img src="./assets/img/cat_des_3.1_mb.png" alt="Imagen de un tractor grande, color verde con llantas negras con rines rojos" class="category_description_img">
-          </div>
-        </div>
       </section>
 
-      <!--BRAND LOGOS-->
-      <section class="brand_carousel">
-        <h2>Nuestro catálogo de marcas</h2>
-        <hr>
-        <section class="carousel">
-          <div class="carousel_list">
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_1_c.jpg" alt="Logo Azteca" class="carousel_img img1">
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_2_c.jpg" alt="Logo Bison" class="carousel_img img2">
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_3_c.jpg" alt="Logo Case In" class="carousel_img img3">
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_4_c.jpg" alt="Logo Famaq" class="carousel_img img4">
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_5_c.jpg" alt="Logo Amssa International" class="carousel_img img5">
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_6_c.jpg" alt="Logo Broch" class="carousel_img img6">
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_7_c.jpg" alt="Logo Jympa" class="carousel_img img7">
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_8_c.jpg" alt="Logo Kimball" class="carousel_img img8">
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_9_c.jpg" alt="Logo New Holland Agriculture" class="carousel_img img9">
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_10_c.jpg" alt="Logo Norte Implementos" class="carousel_img img10"> 
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_11_c.jpg" alt="Logo BKT" class="carousel_img img11">
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_12_c.jpg" alt="Logo Kubota" class="carousel_img img12">
-            </div>
-            <div class="carousel_item">
-              <img src="./assets/img/brands_logos/b_13_c.jpg" alt="Logo Swissmex" class="carousel_img img13">
-            </div>
-          </div>
-        </section>
-      </section>
 
       <!--NOSOTROS-->
       <section class="hero" id=nosotros>
